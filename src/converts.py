cc98adb23924507c057b1113a142e374f9c94aa4
ShyldWolf <randolfcuello@gmail.com>
2025-06-01 01:28:15 -0400

    intengo 99999 de arreglarlo ahora si esta hecho

--- src/converts.py
+++ src/converts.py
@@ -138,13 +138,15 @@ def generate_page(from_path, template_path, dest_path, basepath="/"):
     content_html = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
 
-    html = template.replace("{{ Title }}", title).replace("{{ Content }}", content_html)
-    html = html.replace('href="/', f'href="{basepath}')
-    html = html.replace('src="/', f'src="{basepath}')
+    html = template.replace("{{ Title }}", title)\
+                   .replace("{{ Content }}", content_html)\
+                   .replace("{{ BasePath }}", basepath)
+                  
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(html)
+
 
         
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath="/"):
